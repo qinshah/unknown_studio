5f8a2653bccaa48b96e0d54bc26023d36c1380f9
qshh <3218696007@qq.coms>
2025-08-05 12:08:40 +0800

    feat(文件管理): 添加文件选择器和打开文件功能
    实现文件目录选择和文件打开功能，支持多平台文件操作
    - 添加file_selector和open_file插件依赖
    - 实现目录选择及文件树展示
    - 支持长按打开文件
    - 优化文件树UI显示

--- lib/view/content/content_panel.dart
+++ lib/view/content/content_panel.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart' as m;
 import 'package:flutter_fancy_tree_view2/flutter_fancy_tree_view2.dart';
+import 'package:open_file_ohos/open_file_ohos.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 import '../../model/panel_model.dart';
@@ -14,17 +16,27 @@ class ContentPanel extends StatefulWidget {
 }
 
 class _ContentPanelState extends State<ContentPanel> {
-  final _root = Node(Directory('/'));
-  late final TreeController<Node> _treeController = TreeController(
-    roots: _root.children,
-    childrenProvider: (node) => node.children,
-  );
+  Node? _root;
+  TreeController<Node>? _treeController;
 
   @override
   void initState() {
     super.initState();
-    _loadChildren(_root, loadedDepth: 0, depth: 1).then((_) {
-      _treeController.rebuild();
+  }
+
+  _openCentent() async {
+    var dirPath = await getDirectoryPath();
+    if (dirPath == null) return;
+    setState(() {
+      _root = Node(Directory(dirPath));
+    });
+    _treeController?.dispose();
+    _treeController = TreeController<Node>(
+      roots: _root!.children,
+      childrenProvider: (node) => node.children,
+    );
+    _loadChildren(_root!, loadedDepth: 0, depth: 1).then((_) {
+      _treeController?.rebuild();
     });
   }
 
@@ -64,66 +76,92 @@ class _ContentPanelState extends State<ContentPanel> {
             Text(Panel.content.title),
             Spacer(),
             IconButton.ghost(
-              icon: Icon(Icons.more_horiz),
+              icon: Icon(BootstrapIcons.folder2Open),
               size: ButtonSize.small,
-              onPressed: () {},
-            )
+              onPressed: _openCentent,
+            ),
+            IconButton.ghost(
+              icon: Icon(LucideIcons.folderX),
+              size: ButtonSize.small,
+              onPressed: () => setState(() => _root = null),
+            ),
           ]),
         ),
         Expanded(
-          child: AnimatedTreeView(
-            treeController: _treeController,
-            nodeBuilder: (context, entry) {
-              final node = entry.node;
-              if (!node.childrenLoaded) {
-                _loadChildren(node, depth: 1, loadedDepth: 0).then((_) {
-                  node.childrenLoaded = true;
-                  _treeController.rebuild();
-                });
-              }
-              final entity = entry.node.entity;
-              return m.Material(
-                color: Colors.transparent,
-                child: m.InkWell(
-                  hoverColor: Colors.gray[100],
-                  splashFactory: m.NoSplash.splashFactory, // 禁用涟漪效果
-                  onTap: () => _treeController.toggleExpansion(node),
-                  child: SizedBox(
-                    height: 25,
-                    child: TreeIndentation(
-                      entry: entry,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        physics: NeverScrollableScrollPhysics(),
-                        child: Row(
-                          children: [
-                            AnimatedRotation(
-                              turns: entry.isExpanded ? 0.25 : 0.0,
-                              duration: const Duration(milliseconds: 200),
-                              curve: Curves.easeInOut,
-                              child: Icon(
-                                RadixIcons.caretRight,
-                                size: 16,
-                                color:
-                                    entity is File ? Colors.transparent : null,
+          child: _root == null || _treeController == null
+              ? Center(
+                  child: Button.primary(
+                      onPressed: _openCentent, child: Text('打开目录')),
+                )
+              : AnimatedTreeView(
+                  treeController: _treeController!,
+                  nodeBuilder: (context, entry) {
+                    final node = entry.node;
+                    if (!node.childrenLoaded) {
+                      _loadChildren(node, depth: 1, loadedDepth: 0).then((_) {
+                        node.childrenLoaded = true;
+                        _treeController?.rebuild();
+                      });
+                    }
+                    final entity = entry.node.entity;
+                    return m.Material(
+                      color: Colors.transparent,
+                      child: m.InkWell(
+                        hoverColor: Colors.gray[100],
+                        splashFactory: m.NoSplash.splashFactory, // 禁用涟漪效果
+                        onTap: () => _treeController?.toggleExpansion(node),
+                        onLongPress: () {
+                          if (entity is File) {
+                            OpenFile.open(entity.path);
+                          }
+                        },
+                        child: SizedBox(
+                          height: 25,
+                          child: TreeIndentation(
+                            entry: entry,
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              physics: NeverScrollableScrollPhysics(),
+                              child: Row(
+                                children: [
+                                  entity is File
+                                      ? Icon(
+                                          LucideIcons.dot,
+                                          size: 16,
+                                          color: Colors.gray[400],
+                                        )
+                                      : AnimatedRotation(
+                                          turns: entry.isExpanded ? 0.25 : 0.0,
+                                          duration:
+                                              const Duration(milliseconds: 200),
+                                          curve: Curves.easeInOut,
+                                          child: Icon(
+                                            RadixIcons.caretRight,
+                                            size: 16,
+                                            color: Colors.gray[400],
+                                          ),
+                                        ),
+                                  switch (entity is File
+                                      ? null
+                                      : entry.isExpanded) {
+                                    null => Icon(
+                                        BootstrapIcons.fileEarmarkTextFill,
+                                        color: Colors.gray[400],
+                                      ),
+                                    true => Icon(LucideIcons.folderOpen),
+                                    false => Icon(LucideIcons.folder),
+                                  },
+                                  const SizedBox(width: 2),
+                                  Text(entity.path.split('/').last),
+                                ],
                               ),
                             ),
-                            switch (entity is File ? null : entry.isExpanded) {
-                              null => Icon(RadixIcons.file),
-                              true => Icon(LucideIcons.folderOpen),
-                              false => Icon(LucideIcons.folder),
-                            },
-                            const SizedBox(width: 2),
-                            Text(entity.path.split('/').last),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
         ),
       ],
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
+import open_file_mac
 import screen_retriever_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
